36bb6bb9d24eb620f3f9fa5fd62ac10fcbed209f
Jeff Clegg <jeffclegg@att.net>
2021-01-26 20:13:38 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
@@ -34,6 +32,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/exercise?", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
 
